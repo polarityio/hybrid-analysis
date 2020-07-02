d37be68e8fceea21845f0bcf742772ef4844da01
Ed <ed@polarity.io>
2020-07-01 22:57:28 -0400

    Remove handlebars helper that can't be found

--- components/ha-block.js
+++ components/ha-block.js
@@ -2,10 +2,3 @@
 polarity.export = PolarityComponent.extend({
   details: Ember.computed.alias('block.data.details')
 });
-
-var Handlebars = require('handlebars');
-
-Handlebars.registerHelper("inc", function(value, options)
-{
-    return parseInt(value) + 1;
-});
